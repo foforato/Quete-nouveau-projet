14f76ef126f5af2541c49ff25940ae1c3abbe3d9
foforato <arhim45@gmail.com>
2019-05-13 13:36:13 +0200

    add function show

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,14 +5,39 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
+    /**
+     * @Route("/blog",
+     *     name="blog_index"
+     * )
+     */
+
     public function index()
     {
         return $this->render('blog/index.html.twig', [
             'owner' => 'Aymane',
+        ]);
+    }
+
+    /**
+     * @Route("/blog/show/{article}",
+     *     name="blog_show"
+     * )
+     */
+
+    public function show($article = ' ')
+    {
+        if ($article == ' ') {
+            $slug = "L'article est vide";
+        } else {
+            $slug = ucwords(str_replace('-', ' ', $article));
+        }
+
+        return $this->render('blog/show.html.twig', [
+            'article' => $slug
         ]);
     }
 }
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,9 +5,15 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
+    /**
+     * @Route("/default",
+     *     name="default_index"
+     * )
+     */
     public function index()
     {
         return $this->render('default/default.html.twig');
